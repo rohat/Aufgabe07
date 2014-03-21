05fc7ad03b42fbe30570d9d3e9ba2b105a669b18
Rohat Anil <ranil@student.tgm.ac.at>
2014-03-21 10:28:39 +0100

    commit

---
+++ src/HelloWorld/HelloWorldunso.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+public class HelloWorldunso {
+	public static void helloWorld(){
+		System.out.println("Hallo Arian");
+	}
+	public static void helloWorld2(){
+		System.out.println("Hallo Mondfinsternis");
+	}
+}
